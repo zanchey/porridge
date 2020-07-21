c3a2c52fc9e0898fce65464384a73b6cf5e6c0b9
David Adam <mail@davidadam.com.au>
2020-07-21 22:22:32 +0800

    add source link to about text

--- src/porridge.py
+++ src/porridge.py
@@ -45,6 +45,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Source code for this program is available from
+<https://github.com/zanchey/porridge>."
 """
 
 if hasattr(sys, "_MEIPASS"):
