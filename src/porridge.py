12e8dec391b6565f2581dec631a17fcd04830559
David Adam <mail@davidadam.com.au>
2020-09-21 22:32:03 +0800

    remove stray quotation marks in about text

--- src/porridge.py
+++ src/porridge.py
@@ -47,7 +47,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Source code for this program is available from
-<https://github.com/zanchey/porridge>."
+<https://github.com/zanchey/porridge>.
 """
 
 if hasattr(sys, "_MEIPASS"):
